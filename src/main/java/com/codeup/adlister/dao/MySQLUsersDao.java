f943a653f059888d5eab6d053b123bf0d9fbe4e5
Lineisha <lineishagibson@gmail.com>
2022-07-20 11:20:57 -0500

    work

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -3,6 +3,7 @@ package com.codeup.adlister.dao;
 import com.codeup.adlister.models.User;
 import com.mysql.cj.jdbc.Driver;
 
+import javax.servlet.jsp.jstl.core.Config;
 import java.sql.*;
 
 public class MySQLUsersDao implements Users {
@@ -11,10 +12,7 @@ public class MySQLUsersDao implements Users {
     public MySQLUsersDao(Config config) {
         try {
             DriverManager.registerDriver(new Driver());
-            connection = DriverManager.getConnection(
-                config.getUrl(),
-                config.getUser(),
-                config.getPassword()
+            connection = DriverManager.getConnection( config.getUrl(), config.getUser(), config.getPassword()
             );
         } catch (SQLException e) {
             throw new RuntimeException("Error connecting to the database!", e);
