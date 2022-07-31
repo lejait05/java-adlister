27c0bd2ed1ef7316bf007923397721eb146eeeee
Lineisha <lineishagibson@gmail.com>
2022-07-31 16:38:27 -0500

    fixedLabels

--- src/main/java/com/codeup/adlister/dao/DaoFactory.java
+++ src/main/java/com/codeup/adlister/dao/DaoFactory.java
@@ -2,6 +2,7 @@ package com.codeup.adlister.dao;
 
 public class DaoFactory {
     private static Ads adsDao;
+    private static Users usersDao;
     private static Config config = new Config();
 
     public static Ads getAdsDao() {
@@ -10,6 +11,11 @@ public class DaoFactory {
         }
         return adsDao;
     }
-    private static Users usersDao;
+    public static Users getUsersDao() {
+        if (usersDao == null) {
+            usersDao = new MySQLUsersDao(config);
+        }
+        return usersDao;
+    }
 
 }
--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -1,15 +1,60 @@
 package com.codeup.adlister.dao;
 
 import com.codeup.adlister.models.User;
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.*;
 
 public class MySQLUsersDao implements Users{
+  private Connection connection;
+  public MySQLUsersDao(Config config){
+      try {
+          DriverManager.registerDriver(new Driver());
+          connection = DriverManager.getConnection(config.getUrl(), config.getUser(), config.getPassword());
+      }catch(SQLException e){
+          throw new RuntimeException("Error connecting to the db!", e);
+      }
+  }
     @Override
     public User findByUsername(String username) {
-        return null;
+      String  query =  "SELECT * FROM users WHERE username = ? LIMIT 1";
+      try{
+          PreparedStatement statement = connection.prepareStatement(query);
+          statement.setString(1,username);
+          return extractUser(statement.executeQuery());
+      }catch (SQLException e){
+          throw  new RuntimeException("Error finding user by username", e);
+      }
+
+
     }
 
     @Override
     public Long insert(User user) {
-        return null;
-    }
+       String query = "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
+        try {
+            PreparedStatement statement= connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, user.getUsername());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, user.getPassword());
+            statement.executeUpdate();
+            ResultSet rs = statement.getGeneratedKeys();
+            rs.next();
+            return rs.getLong(1);
+    }catch (SQLException e){
+            throw new RuntimeException("Error creating new user", e);
+        }
+}
+private User extractUser(ResultSet rs) throws SQLException{
+  if(!rs.next()){
+  return null;
+  }
+  return new User(
+          rs.getLong("id"),
+          rs.getString("username"),
+          rs.getString("email"),
+          rs.getString("password")
+  );
+
+}
 }
